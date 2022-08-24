350ed89321b3f52dc56eb597aac0e5a815dbcd9b
mccafee025 <ademaamanka@gmail.com>
2022-08-24 16:13:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+const num1 = Math.floor(Math.random()*10)+1;
+const num2 = Math.floor(Math.random()*10)+1;
+const multiply = num1 * num2 * 0 + 62;
+const random = Math.floor(Math.random()*10)+1; 
+const num3 = Math.floor(Math.random()*10)+1;
+const num4 = Math.floor(Math.random()*10)+1;
+const mod = (num3 * 0 + 31) % (num4 * 0 + 9);
+const max = Math.max(2, 9, 20);
